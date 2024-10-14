ebdfb43ef4a65ced31ef89f7cee95595587affdf
Maria Fernanda <mfnanda.ribeiro@gmail.com>
2024-10-14 16:53:11 -0300

    continuação

---
+++ vue-app/src/components/info.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <p v-if="esta_trabalhando">Estou trabalhando no momento.</p>
+        <p v-else>Estou em busca de novas oportunidades!</p>
+        <p>E utilizo as seguintes tecnologias:</p>
+        <ul>
+            <li>JavaScript</li>
+            <li>PHP</li>
+            <li>Python</li>
+        </ul>
+        <p v-if="4 > 2">testando</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Info',
+    data() {
+        return {
+            esta_trabalhando: true
+        }
+    }
+}
+</script>
